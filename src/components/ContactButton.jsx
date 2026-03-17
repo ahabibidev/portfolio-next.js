79bd359a415d8f130abdf118643193ff102e0d07
Ali Reza Habibi <shahreyarhabibi@gmail.com>
2026-03-17 13:28:49 +0430

    Fixed sizing in mobile device

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -7,8 +7,8 @@ export default function ContactButton({ children, width, ...props }) {
   return (
     <a
       {...props}
-      href="mailto:shahreyarhabibi@gmail.com"
-      className={`inline-flex items-center gap-2 text-sm md:text-lg md:px-6 md:py-3 px-5 py-2.5 border-2 
+      href="mailto:ahabibidev@gmail.com"
+      className={`gap-2 text-sm md:text-lg md:px-6 md:py-3 px-5 py-2.5 border-2 
             border-(--primary) text-(--primary) rounded-full font-medium
             transition-all duration-300 hover:bg-(--primary) hover:text-white`}
     >
