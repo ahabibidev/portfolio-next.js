1b6b4a84ef01b7542d0d396b945495ca5e2bdded
Ali Reza Habibi <shahreyarhabibi@gmail.com>
2026-04-20 16:19:11 +0430

    SEO Optimization

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -7,7 +7,7 @@ export default function ContactButton({ children, width, ...props }) {
   return (
     <a
       {...props}
-      href="mailto:ahabibidev@gmail.com"
+      href="mailto:contact@ahabibi.dev"
       className={`gap-2 text-sm md:text-lg md:px-6 md:py-3 px-5 py-2.5 border-2 
             border-(--primary) text-(--primary) rounded-full font-medium
             transition-all duration-300 hover:bg-(--primary) hover:text-white`}
